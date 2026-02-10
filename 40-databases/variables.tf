cc07129bc28f994f34c2b10d3bcb5f6ca0c16670
lakshmi <lakshminarasimha228@gmail.com>
2026-02-10 19:16:25 +0530

    terraform

---
+++ 40-databases/variables.tf
@@ -0,0 +1,15 @@
+variable "project" {
+  default = "roboshop"
+}
+
+variable "environment" {
+  default = "dev"
+}
+
+variable "zone_id" {
+    default = "Z08607771ZIU55CBS5TGT"
+}
+
+variable "zone_name" {
+    default = "lakshmi.cyou"
+}
