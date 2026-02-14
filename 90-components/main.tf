c2fde58edb011dbd34eb7a39911728408c10dc16
lakshmi <lakshminarasimha228@gmail.com>
2026-02-15 00:58:00 +0530

    terraform

--- 90-components/main.tf
+++ 90-components/main.tf
@@ -1,6 +1,6 @@
 module "component" {
     for_each = var.components
-    source = "git::https://github.com/Lakshminarasimha228/terraform-aws-roboshop.git?ref=main"
+    source = "git::https://github.com/Lakshminarasimha228/terraform-aws-roboshop.git?ref=main"  #terraform-aws-roboshop
     component = each.key
     rule_priority = each.value.rule_priority
-}
+} 
